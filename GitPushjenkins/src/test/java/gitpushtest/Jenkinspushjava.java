f92a003f58afa15dee5f5d2e7ef908140cb9da3e
LT-SUKR <sureshisoft@gmail.com>
2022-07-08 09:38:53 +0530

    first commit

---
+++ GitPushjenkins/src/test/java/gitpushtest/Jenkinspushjava.java
@@ -0,0 +1,11 @@
+package gitpushtest;
+
+public class Jenkinspushjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First line suresh");
+	}
+
+}
